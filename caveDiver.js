ae0aa3b95ebe9f38d78456fd8d4b03026fac4c19
ioniklabs <matt@ioniklabs.com>
2016-01-18 23:52:38 +1000

    Update caveDiver.js
    restructure plugin

--- caveDiver.js
+++ caveDiver.js
@@ -1,99 +1,150 @@
 var caveDiver = ( function( document, window ) {
 
-	function caveDiver() {
+	function caveDiver( options ) {
 		this.options = {
-			log: false,
-			compare_removed: true,
-			compare_added: true,
-			compare_modified: true,
-			apply_removed: true,
-			apply_added: true,
-			apply_modified: true,
-			cb: {
-				remove: function( id ) { console.log( 'remove', id ); },
-				add: function( id, obj ) { console.log( 'add', id, obj ); },
-				modify: function( id, obj1, obj2 ) { console.log( 'modify', id, obj ); }
+			return_id_array: false,
+			return_object_array: true,
+			return_clean_object_array: false,
+			callback: {
+				id_lhs: function( lhs ) {
+					return lhs.id;
+				},
+				id_rhs: function( rhs ) {
+					return rhs.id;
+				},
+				return_lhs: function( lhs ) {
+					return lhs;
+				},
+				return_rhs: function( rhs ) {
+					return rhs;
+				},
+				match: function( lhs, rhs ) {
+					return lhs.id === rhs.id;
+				},
+				compare: function( lhs, rhs ) {
+					return lhs.timestamp === rhs.timestamp;
+				},
+				/*
+				|
+				|	todo: apply method?
+				|
+				| * /
+				add: function() {
+
+				},
+				remove: function() {
+
+				},
+				apply: function() {
+
+				}
+				/* */
+			}
+		};
+		if ( is__object( options ) ) this.options = extend( this.options, options );
+	};
+
+	caveDiver.prototype.update__options = function( options ) {
+		if ( !is__object( options ) ) return on__error( 103 );
+		this.options = extend( this.options, options );
+	};
+
+	caveDiver.prototype.compare = function( lhs, rhs ) {
+		if ( !is__array( lhs ) ) on__error( 101 );
+		if ( !is__array( rhs ) ) on__error( 102 );
+		var lhs_id = [],
+			rhs_id = [];
+
+		var response = {};
+
+		if ( this.options.return_id_array ) {
+			extend( response, {
+				add__id_array: [],
+				remove__id_array: [],
+				modify__id_array: []
+			} );
+		}
+
+		if ( this.options.return_object_array ) {
+			extend( response, {
+				add__obj_array: [],
+				remove__obj_array: [],
+				modify__obj_array: []
+			} );
+		}
+
+		lhs_id = get__flat_id_array( lhs, this.options.callback.id_lhs );
+		rhs_id = get__flat_id_array( rhs, this.options.callback.id_rhs );
+
+		var index = 0;
+			length = lhs_id.length,
+			match = false;
+
+		for( index; index < length; index++ ) {
+			if ( rhs_id.indexOf( lhs_id[index] ) === -1 ) {
+
+				add__response( 'lhs' )
+
+				if ( this.options.return_id_array ) response.remove__id_array.push( lhs_id[index] );
+				if ( this.options.return_object_array ) response.remove__obj_array.push( this.options.return_clean_object_array ? this.options.callback.return_lhs( lhs[index] ) : lhs[index] );
+			} else {
+				match = find__match( lhs[index], rhs, this.options.callback.match );
+				if ( is__object( match ) ) {
+					if ( !this.options.callback.compare( lhs[index], match ) ) {
+						if ( this.options.return_id_array ) response.modify__id_array.push( rhs_id[index] );
+						if ( this.options.return_object_array ) {
+							response.modify__obj_array.push( {
+								lhs: this.options.return_clean_object_array ? this.options.callback.return_lhs( lhs[index] ) : lhs[index],
+								rhs: this.options.return_clean_object_array ? this.options.callback.return_rhs( rhs[index] ) : rhs[index]
+							} );
+						}
+					}
+				}
 			}
 		}
+
+		var index = 0;
+			length = rhs_id.length;
+
+		for( index; index < length; index++ ) {
+			if ( lhs_id.indexOf( rhs_id[index] ) === -1 ) {
+				if ( this.options.return_id_array ) response.add__id_array.push( rhs_id[index] );
+				if ( this.options.return_object_array ) response.add__obj_array.push( this.options.return_clean_object_array ? this.options.callback.return_rhs( rhs[index] ) : rhs[index] );
+			}
+		}
+
+		return response;
 	};
 
-	caveDiver.prototype.compare = function( base_array, comparison_array, key ) {
-		return this.get__diff( base_array, comparison_array, key );
+	caveDiver.prototype.find__match = function( obj, array, predicate ) {
+		return find__match( obj, array, predicate );
 	};
 
-		caveDiver.prototype.compareApplyAll = function( base_array, comparison_array, key ) {
-			var log = this.get__diff( base_array, comparison_array, key );
-			this.applyAll( log, base_array, comparison_array, key );
-		};
+	function add__response() {
 
-			caveDiver.prototype.applyAll = function( log, base_array, comparison_array, key ) {
-				if ( this.options.apply_removed ) this.applyRemoved( log['removed'], key );
-				if ( this.options.apply_added ) this.applyAdded( log['added'], comparison_array, key );
-				if ( this.options.apply_modified ) this.applyModified( log['modified'], base_array, comparison_array, key );
-			};
+	};
 
-				caveDiver.prototype.applyRemoved = function( log, key ) {
-					var log_ = extract__log( log, 'removed' );
-					this.apply_( log_, this.options.cb.remove );
-				};
+	function get__flat_id_array( array, callback ) {
+		var index = 0;
+			length = array.length,
+			array_id = [],
+			unique_id = null;
+		for ( index; index < length; index++ ) {
+			unique_id = callback(array[index]);
+			if ( !is__valid_type( unique_id, ['string', 'number'] ) ) return on__error( 201 );
+			if ( array_id.indexOf( unique_id ) !== -1 ) return on__error( 202 );
+			array_id.push( unique_id );
+		}
+		return array_id;
+	};
 
-				caveDiver.prototype.applyAdded = function( log, comparison_array, key ) {
-					var log_ = extract__log( log, 'added' );
-					this.apply_( log_, this.options.cb.add, key, comparison_array );
-				};
-
-				caveDiver.prototype.applyModified = function( log, base_array, comparison_array, key ) {
-					var log_ = extract__log( log, 'modified' );
-					this.apply_( log_, this.options.cb.modify, key, comparison_array );
-				};
-
-	caveDiver.prototype.apply_ = function( array, callback, key, return_obj_array_1, return_obj_array_2 ) {
+	function find__match( obj, array, predicate ) {
 		var index = 0,
 			length = array.length;
 		for ( index; index < length; index++ ) {
-			callback( array[index], is__array( return_obj_array_1 ) ? this.get__obj_by_id( return_obj_array_1, key, array[index] ) : undefined, is__array( return_obj_array_2 ) ? this.get__obj_by_id( return_obj_array_2, key, array[index] ) : undefined );
+			if ( predicate( obj, array[index] ) ) return array[index];
 		}
-	};
-
-	caveDiver.prototype.get__diff = function( base_array, comparison_array, key ) {
-
-		var response = {
-			removed: [],
-			modified: [],
-			added: []
-		},
-		base_ids = [],
-		comparison_ids = [];
-
-		if ( !is__array( base_array ) ) {
-			return on__error(101);
-		}
-
-		if ( !is__array( comparison_array ) ) {
-			return on__error(102);
-		}
-
-		if ( !is__valid_type( key, ['string', 'number'] ) ) {
-			return on__error(103);
-		}
-
-		base_ids = get__ids( base_array, key );
-		comparison_ids = get__ids( comparison_array, key );
-
-		if ( this.options.compare_removed ) response.removed = get__array_diff( base_ids, comparison_ids );
-		if ( this.options.compare_added ) response.added = get__array_diff( comparison_ids, base_ids );
-		//  if ( this.options.compare_modified ) response.modified = get__array_modifications( comparison_ids, base_ids );
-
-		return response;
-
-	};
-
-	caveDiver.prototype.get__index_by_id = function( array, key, id ) {
-		return get__index_by_id( array, key, id );
-	};
-
-	caveDiver.prototype.get__obj_by_id = function( array, key, id ) {
-		return array[get__index_by_id( array, key, id )];
+		return {};
 	};
 
 	/*
@@ -102,89 +153,49 @@ var caveDiver = ( function( document, window ) {
 	|
 	| */
 
-	function extract__log( log, key ) {
-		if ( is__array( log ) ) return log;
-		if ( is__array( log[key] ) ) return log[key];
-		return on__error( 301 );
-	};
-
-	function is__array( obj ) {
-		return typeof obj === 'object' && typeof obj.length === 'number' ? true : false;
-	};
-
-	function is__object( obj ) {
-		return typeof obj === 'object' && typeof obj.length === 'undefined' ? true : false;
-	};
-
-	function is__valid_type( obj, valid_types ) {
-		return valid_types.indexOf( typeof obj ) !== -1 ? true : false;
-	};
-
-	function get__ids( array, key ) {
-		var ids = [],
-			index = 0,
-			length = array.length;
-		for ( index; index < length; index++ ) {
-			key_val = array[index][key];
-			if ( !is__valid_type( key_val, ['string', 'number'] ) ) return on__error( 201 );
-			if ( ids.indexOf( key_val ) !== -1 ) return on__error( 202 );
-			ids.push( key_val );
+	function extend( obj_1, obj_2 ) {
+		for (var key in obj_2) {
+			obj_1[key] = obj_2[key];
 		}
-		return ids;
+		return obj_1;
 	};
 
-	function get__array_diff( array_1, array_2 ) {
-		var diffs = [],
-			index = 0,
-			length = array_1.length;
-		for( index; index < length; index++ ) {
-			if ( array_2.indexOf( array_1[index] ) === -1 ) {
-				diffs.push( array_1[index] );
+	function is__array( variable ) {
+		return variable instanceof Array ? true : false;
+	};
+
+	function is__object( variable ) {
+		return variable instanceof Object ? true : false;
+	};
+
+	function is__function( variable ) {
+		return variable instanceof Function ? true : false;
+	};
+
+	function is__valid_type( variable, valid_types ) {
+		return valid_types.indexOf( typeof variable ) !== -1 ? true : false;
+	};
+
+	function clone( variable ) {
+		if ( is__array( variable ) ) {
+			var copy = [],
+				index = 0,
+				length = variable.length;
+			for ( index; index < length; index++ ) {
+				copy[index] = clone(variable[index]);
 			}
+			return copy;
 		}
-		return diffs;
-	};
 
-	function get__array_duplicates( array_1, array_2 ) {
-		var dups = [],
-			index = 0,
-			length = array_1.length;
-		for( index; index < length; index++ ) {
-			if ( array_2.indexOf( array_1[index] ) !== -1 ) {
-				dups.push( array_1[index] );
+		if ( is__object( variable ) ) {
+			var copy = {};
+			for ( var attr in variable ) {
+				if ( variable.hasOwnProperty( attr ) ) copy[attr] = clone( variable[attr] );
 			}
+			return copy;
 		}
-		return dups;
-	};
 
-	function get__array_modifications( array_1, array_2 ) {
-		var mods = [],
-			dups = get__array_duplicates( array_1, array_2 ),
-			index = 0,
-			length = array_1.length;
-		for( index; index < length; index++ ) {
-			if ( dups.indexOf( array_1[index] ) !== -1 ) {
-				// mods.push( array_1[index] );
-				/* compare */
-			}
-		}
-		return mods;
-	};
-
-	function get__index_by_id( array, key, id ) {
-		var index = 0,
-			index_at = -1;
-			length = array.length;
-		for ( index; index < length; index++ ) {
-			if ( array[index][key] == id ) {
-				if ( index_at === -1 ) {
-					index_at = index;
-				} else {
-					return this.on__error( 202 );
-				}
-			}
-		}
-		return index_at;
+		return variable;
 	};
 
 	/*
@@ -203,33 +214,17 @@ var caveDiver = ( function( document, window ) {
 	function get__error_msg( id, msg ) {
 		var error_msg;
 		switch( id ) {
-			case 101: error_msg = 'invalid array: base_array'; break;
-			case 102: error_msg = 'invalid array: comparison_array'; break;
-			case 103: error_msg = 'invalid key'; break;
+			case 101: error_msg = 'invalid array'; break;
+			case 102: error_msg = 'invalid array'; break;
+			case 103: error_msg = 'invalid object'; break;
 			case 201: error_msg = 'invalid or undefined key-value'; break;
 			case 202: error_msg = 'found multiple matching key-value pairs'; break;
-			case 301: error_msg = 'invalid log passed to an apply method'; break;
 			case 0:
 			default:
 				error_msg = 'unknown';
 		}
 		return error_msg;
 	};
-
-	/*
-	|
-	|
-	|
-	| * /
-
-	caveDiver.prototype.extend = function() {
-		for (var key in this.settings) {
-			this.defaults[key] = this.settings[key];
-		}
-		return this.op;
-	};
-
-	/* /.extend */
 
 	/*
 	|
